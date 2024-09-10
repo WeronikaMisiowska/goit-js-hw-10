61c2dab27b4b60010fd1ee32275363697d2641c6
WeronikaMisiowska <167077873+WeronikaMisiowska@users.noreply.github.com>
2024-09-10 09:36:31 +0200

    Done

---
+++ src/js/02-snackbar.js
@@ -0,0 +1,35 @@
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  
+  const delay = Number(event.target.delay.value);
+  const state = event.target.state.value;
+
+ 
+  const createPromise = (delay, state) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+  };
+
+  
+  createPromise(delay, state)
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
